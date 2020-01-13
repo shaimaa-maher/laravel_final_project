df9b1388cf333e33120cade7c097d7da6d40daf2
EsraaAldih <esraa.aldih123@gmail.com>
2020-01-13 19:49:28 +0200

    store users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,20 +25,29 @@ class UserController extends Controller
          if(request()->has('avatar')){
             $user = new User;
 
-            $avatar = $request->file('avatar');
-            $avatar_name = time().'.'.$avatar->getClientOriginalExtension();
-            $avatar_path = public_path('bower_components/AdminLTE/dist/img/');
-            $avatar->move($avatar_path,$avatar_name);
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->role =$request->role;
-        $user->password = Hash::make($request->password);
-        $user->avatar_url = 'bower_components/AdminLTE/dist/img/'.$avatar_name;
+            // $file = $request->file('avatar');
+            // //make sure yo have image folder inside your public
+            // $destination_path = '/home/esraa/Desktop/project_laravel/public/bower_components/AdminLTE/dist/img/';
+            // $profileImage = date("Ymd").".".$file->getClientOriginalName();
+            // $file->move($destination_path, $profileImage);
+            // //save the link of thumbnail into myslq database        
+            // $user->avatar_url = $destination_path . $profileImage;
+          
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->role =$request->role;
+            $user->password = Hash::make($request->password);
         
-        $user->save();
+            $user->save();
             
         }
-        
+        return User::create([
+           
+            $user->name = $request->name,
+            $user->email = $request->email,
+            $user->role =$request->role,
+            $user->password = Hash::make($request->password),
+        ]);
         return redirect()->route('users.index');
     }
 
